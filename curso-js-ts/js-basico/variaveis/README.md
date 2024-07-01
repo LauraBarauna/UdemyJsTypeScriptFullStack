5ebbc81692651ed8546beb1c0c3a1970b1de7dbb
LauraBarauna <lauravbarauna@gmail.com>
2024-07-01 17:37:20 -0300

    Criando README com regras das variáveis e o arquivo sobre let

---
+++ curso-js-ts/js-basico/variaveis/README.md
@@ -0,0 +1,9 @@
+# Regras de variáveis
+1. Não podemos criar variáveis com palavras reservadas
+2. Variáveis precisam ter nomes significativos
+3. Não pode começar o nome de uma variável com um número
+4. Não podem conter espaços ou traços
+5. Utilizamos **camelCase**
+6. Case-sensitive
+7. Não podemos redeclarar variáveis com let
+8. **NÃO UTILIZE VAR, UTILIZE LET.**
--- curso-js-ts/js-basico/variaveis/let/index.js
+++ curso-js-ts/js-basico/variaveis/let/index.js
@@ -1,5 +1,29 @@
-console.log("João nasceu em 1984");
-console.log("Em 2000, João conheceu Maria");
-console.log("João casou-se com Maria em 2012");
-console.log("Maria teve 1 filho com João em 2015");
-console.log("O filho de João se chama Eduardo");
+let nome; // Declarou a variável
+nome = "João"; // Inicializando a variável
+
+console.log(nome, "nasceu em 1984.");
+console.log("Em 2000,", nome, "conheceu Maria.");
+console.log(nome, "casou-se com Maria em 2012.");
+console.log("Maria teve 1 filho com João em 2015.");
+console.log("O filho de" , nome, "se chama Eduardo.");
+
+console.log("========")
+
+nome = "Mario"; // Mudando o valor da variável
+console.log(nome, "nasceu em 1984.");
+console.log("Em 2000,", nome, "conheceu Maria.");
+console.log(nome, "casou-se com Maria em 2012.");
+console.log("Maria teve 1 filho com João em 2015.");
+console.log("O filho de" , nome, "se chama Eduardo.");
+
+
+/* Regras
+ * Não podemos criar variáveis com palavras reservadas;
+ * Variáveis precisam ter nomes significativos
+ * Não pode começar o nome de uma variável com um número
+ * Não podem conter espaços ou traços
+ * Utilizamos camelCase
+ * Case-sensitive
+ * Não podemos redeclarar variáveis com let
+ * NÃO UTILIZE VAR, UTILIZE LET.
+ */
