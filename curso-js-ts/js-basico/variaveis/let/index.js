35c8e2fab50564e1df404460d4b78cf15991047c
LauraBarauna <lauravbarauna@gmail.com>
2024-07-01 17:39:28 -0300

    Retirando as regras das variáveis que estava dentro do arquivo de let

--- curso-js-ts/js-basico/variaveis/let/index.js
+++ curso-js-ts/js-basico/variaveis/let/index.js
@@ -17,13 +17,3 @@ console.log("Maria teve 1 filho com João em 2015.");
 console.log("O filho de" , nome, "se chama Eduardo.");
 
 
-/* Regras
- * Não podemos criar variáveis com palavras reservadas;
- * Variáveis precisam ter nomes significativos
- * Não pode começar o nome de uma variável com um número
- * Não podem conter espaços ou traços
- * Utilizamos camelCase
- * Case-sensitive
- * Não podemos redeclarar variáveis com let
- * NÃO UTILIZE VAR, UTILIZE LET.
- */
