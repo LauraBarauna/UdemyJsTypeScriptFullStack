8eb7b3bc4bf2ef7f94abb012819310147de44f95
Laura Barauna <lauravbarauna@gmail.com>
2024-07-11 18:26:33 -0300

    Finalizando parte do script do exercício de Numbers e Objeto Math

--- curso-js-ts/js-basico/numbers/exercicio/index.js
+++ curso-js-ts/js-basico/numbers/exercicio/index.js
@@ -1,1 +1,30 @@
-alert("Teste")
+const numero = Number(prompt("Digite um número")); 
+const raizQuadrada = numero ** 0.5;
+let respostas;
+
+function NumeroInteiro(){
+    if(Number.isInteger(numero)){
+        return "Sim";
+    }else{
+        return "Não";
+    }
+}
+
+function NotANumber(){
+    if(isNaN(numero)){
+        return "Sim";
+    }else{
+        return "Não";
+    }
+}
+
+const arredondandoBaixo = Math.floor(numero);
+const arredondandoCima = Math.ceil(numero);
+const duasCasasDecimais = numero.toFixed(2);
+
+numeroTitulo = document.getElementById("numeroTitulo").innerHTML = `O número é ${numero}`
+perguntas = document.getElementById("perguntas").innerHTML = `Raiz quadrada: ${raizQuadrada} <br/> 
+${numero} é inteiro? ${NumeroInteiro()}<br/> É NaN? ${NotANumber()} <br/> 
+Arredondando pra baixo: ${arredondandoBaixo} <br/>
+Arredondando pra cima: ${arredondandoCima} <br/>
+Com duas casas decimais: ${duasCasasDecimais}`
