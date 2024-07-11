1efbe913133ba2b101a8ce4fd7e007576ad34f4c
Laura Barauna <lauravbarauna@gmail.com>
2024-07-11 17:56:15 -0300

    Finalizando aula do Objeto Math

---
+++ curso-js-ts/js-basico/numbers/objeto-math/index.js
@@ -0,0 +1,9 @@
+let num1 = 9.78593;
+
+// let num2 = Math.floor(num1); // Arredonda pra baixo
+// let num2 = Math.ceil(num1); // Arredonda pra cima
+// let num2 = Math.round(num1); // Arredonda automaticamente, se for metade ou acima da metade, arredonda pra cima, se for menor que a metade arredonda pra baixo
+// console.log(Math.max(1,2,3,4,5,-10,-50,1500,9,8,7,6)); // Retorna o maior número da sequência
+// console.log(Math.min(1,2,3,4,5,-10,-50,1500,9,8,7,6)); // Retorna o menor número da sequência
+// const aleatorio = Math.round(Math.random() * (10 - 5) + 5); // Retorna um número aleatório entre 10 e 5
+
