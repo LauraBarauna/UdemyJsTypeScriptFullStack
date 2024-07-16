1cd09456de1dcde959c62ad8c65578742cec9a34
Laura Barauna <lauravbarauna@gmail.com>
2024-07-16 16:31:43 -0300

    Finalizando a aula sobre operadores lógicos

---
+++ curso-js-ts/logica-de-programacao/operadores-lógicos/idnex.js
@@ -0,0 +1,10 @@
+/*
+Operadores Lógicos
+&& And e
+|| ou
+! não
+*/
+
+console.log(true && true); // Todas precisam ser verdadeira, caso um seja falso, a expressão toda sera falsa
+console.log(true || false); // Apenas uma precisa ser verdadeira
+console.log(!true); // Nega (Inverte) uma expressão
