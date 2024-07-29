6e404d053257de6597fd2ebba9cbc56fecaf9b6b
Laura Barauna <lauravbarauna@gmail.com>
2024-07-29 20:20:19 -0300

    Finalizando aula sobre for clássico

---
+++ curso-js-ts/logica-de-programacao/for-classico/index.js
@@ -0,0 +1,17 @@
+// For clássico
+
+for(let i = 400; i <= 500; i++) {
+    console.log(i);
+}
+
+console.log("Aumentando de 10 em 10");
+
+for(let i = 400; i <= 500; i += 10) {
+    console.log(i);
+}
+
+console.log("Modo decrescente");
+
+for(let i = 500; i>=400; i--) {
+    console.log(i);
+}
