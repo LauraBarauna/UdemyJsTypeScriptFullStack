174fe46d40f0a4d59fd56c826bca026981081419
Laura Barauna <lauravbarauna@gmail.com>
2024-09-03 16:59:51 -0300

    Finalizando aula sobre for of

---
+++ curso-js-ts/logica-de-programacao/for-of/index.js
@@ -0,0 +1,10 @@
+const nome = 'Laura Barauna';
+const sobrenomes = ['Barauna', 'Bueno', 'Veiga'];
+
+for (let valor of nome){
+    console.log(valor);
+}
+
+for (let valor of sobrenomes){
+    console.log(valor);
+}
