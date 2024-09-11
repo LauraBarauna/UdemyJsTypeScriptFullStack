ba5a6665d413341fe8a56076bee67911ad5f0af3
Laura Barauna <lauravbarauna@gmail.com>
2024-09-11 17:36:07 -0300

    Finalizando exercício sobre lógica

--- curso-js-ts/logica-de-programacao/exercicios-logica-de-programacao/exercicio-2/index.js
+++ curso-js-ts/logica-de-programacao/exercicios-logica-de-programacao/exercicio-2/index.js
@@ -1,4 +1,4 @@
-// Escreva uma função chama ePaisagem que
+// Escreva uma função chamada ePaisagem que
 // recebe dois argumentos, largura e altura
 // de uma imagem (number).
 // Retorne true se a imagem estiver no modo paisagem.
