05370c8eb863b1f04f7c01a74298a949345dce5d
Laura Barauna <lauravbarauna@gmail.com>
2024-11-05 19:41:15 -0300

    Finalizando aula sobre fethc API e axios com JSON

---
+++ curso-js-ts/js-assincrono/fetch-api-axios-json/assets/js/mains.js
@@ -0,0 +1,61 @@
+
+// Fetch
+// fetch('pessoas.json')
+//     .then(resposta => resposta.json())
+//     .then(json => carregaElementosNaPagina(json));
+
+// function carregaElementosNaPagina(json) {
+//     const table = document.querySelector('.tabela');
+
+//     for(let pessoa of json) {
+//         const tr = document.createElement('tr');
+
+//         let td = document.createElement('td');
+//         td.innerHTML = pessoa.nome;
+//         tr.appendChild(td);
+
+//         td = document.createElement('td');
+//         td.innerHTML = pessoa.idade;
+//         tr.appendChild(td);
+
+//         td = document.createElement('td');
+//         td.innerHTML = pessoa.salario;
+//         tr.appendChild(td);
+
+//         table.appendChild(tr);
+//     }
+    
+//     const resultado = document.querySelector('.container');
+//     resultado.appendChild(table);
+
+// }
+
+// Axios
+axios('pessoas.json')
+    .then(resposta => carregaElementosNaPagina(resposta.data))
+
+function carregaElementosNaPagina(json) {
+    const table = document.querySelector('.tabela');
+
+    for(let pessoa of json) {
+        const tr = document.createElement('tr');
+
+        let td = document.createElement('td');
+        td.innerHTML = pessoa.nome;
+        tr.appendChild(td);
+
+        td = document.createElement('td');
+        td.innerHTML = pessoa.idade;
+        tr.appendChild(td);
+
+        td = document.createElement('td');
+        td.innerHTML = pessoa.salario;
+        tr.appendChild(td);
+
+        table.appendChild(tr);
+    }
+    
+    const resultado = document.querySelector('.container');
+    resultado.appendChild(table);
+
+}
