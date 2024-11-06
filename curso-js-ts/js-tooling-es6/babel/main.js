f862cc2a61d8c391ceb975889943789485365232
Laura Barauna <lauravbarauna@gmail.com>
2024-11-06 16:01:09 -0300

    Finalizando aula sobre babel

--- curso-js-ts/js-tooling-es6/babel/main.js
+++ curso-js-ts/js-tooling-es6/babel/main.js
@@ -1,4 +1,6 @@
-const nome =  'Laura';
-const obj = {nome};
-const novoObj = { ...obj};
-console.log(novoObj);  
+class Pessoa {
+    constructor(nome, sobrenome) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+    }
+}
