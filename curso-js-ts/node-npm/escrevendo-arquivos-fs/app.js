f334ad2c4397253a448b79470964caf6fe81f9d9
Laura Barauna <lauravbarauna@gmail.com>
2024-11-15 10:32:22 -0300

    Finalizando aula sobre como escrever e ler arquivos usando fs

---
+++ curso-js-ts/node-npm/escrevendo-arquivos-fs/app.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const caminhoArquivo = path.resolve(__dirname, 'teste.json');
+const escrever = require('./modules/escreve');
+const ler = require('./modules/ler');
+
+const pessoas = [
+  { nome: 'João' },
+  { nome: 'Maria' },
+  { nome: 'Eduardo' },
+  { nome: 'Luiza' },
+];
+const json = JSON.stringify(pessoas, '', 2);
+escrever(caminhoArquivo, json);
+
+async function leArquivo(caminho) {
+  const dados = await ler(caminho);
+  renderizaDados(dados);
+}
+
+function renderizaDados(dados) {
+  dados = JSON.parse(dados);
+  dados.forEach(val => console.log(val.nome));
+}
+leArquivo(caminhoArquivo);
