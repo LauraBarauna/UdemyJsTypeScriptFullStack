df962556e965433958107b7421b1232521ce2c88
Laura Barauna <lauravbarauna@gmail.com>
2024-11-25 19:48:00 -0300

    Finalizando aula sobre routers e cotrollers

--- curso-js-ts/node-npm/express/routers-controllers/server.js
+++ curso-js-ts/node-npm/express/routers-controllers/server.js
@@ -1,27 +1,9 @@
 const express = require('express');
 const app = express();
+const routes = require('./routes');
 
 app.use(express.urlencoded({extended: true }));
-
-app.get('/', (req, res) => {
-    res.send(`
-        <form action="/" method="POST">
-        nome do cliente: <input type="text" name="nome">
-        <button>Olá mundo</button>
-        </form>
-        `);
-});
-
-app.post('/', (req, res) => {
-    console.log(req.body);
-    res.send(`O que você me enviou foi: ${req.body.nome}`);
-});
-
-app.get('/testes/:idUsuarios?/:paramentro?', (req, res) => {
-    console.log(req.params);
-    console.log(req.query);
-    res.send(req.query.idade);
-})
+app.use(routes); 
 
 app.listen(3000, () => {
     console.log('Servidor executando na porta 3000')
