e7618c070204481156ed48c34aeee3ece50bb168
Laura Barauna <lauravbarauna@gmail.com>
2024-11-25 17:38:35 -0300

    Finalizando aula sobre req.params req.query e req.body

--- curso-js-ts/node-npm/express/res-params-query-body/server.js
+++ curso-js-ts/node-npm/express/res-params-query-body/server.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const app = express();
 
+app.use(express.urlencoded({extended: true }));
+
 app.get('/', (req, res) => {
-    res.send('Olá Mundo!');
+    res.send(`
+        <form action="/" method="POST">
+        nome do cliente: <input type="text" name="nome">
+        <button>Olá mundo</button>
+        </form>
+        `);
 });
 
-app.get('/contato', (req, res) => {
-    res.send('Obrigada por entrar em contato com a gente.');
+app.post('/', (req, res) => {
+    console.log(req.body);
+    res.send(`O que você me enviou foi: ${req.body.nome}`);
+});
+
+app.get('/testes/:idUsuarios?/:paramentro?', (req, res) => {
+    console.log(req.params);
+    console.log(req.query);
+    res.send(req.query.idade);
 })
 
 app.listen(3000, () => {
