cc1380e98e1e8a642bdd1fad0c3fcdef8c046dd4
Laura Barauna <lauravbarauna@gmail.com>
2024-11-26 16:23:46 -0300

    Finalizando aula sobre arquivo estático

--- curso-js-ts/node-npm/express/arquivos-estaticos/server.js
+++ curso-js-ts/node-npm/express/arquivos-estaticos/server.js
@@ -4,6 +4,9 @@ const routes = require('./routes');
 const path = require('path');
 
 app.use(express.urlencoded({extended: true }));
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+
 app.set('views', path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
 
