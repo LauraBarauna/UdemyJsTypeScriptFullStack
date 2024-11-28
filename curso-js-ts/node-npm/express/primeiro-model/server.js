9a00bcde6f113f2c9d614ee70b2750992c64169e
Laura Barauna <lauravbarauna@gmail.com>
2024-11-28 17:28:46 -0300

    Criando aula sobre primeiro model e coneção mongo db

--- curso-js-ts/node-npm/express/primeiro-model/server.js
+++ curso-js-ts/node-npm/express/primeiro-model/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const routes = require('./routes');
 const path = require('path');
+const { middlewareGlobal, outroMiddleware } = require('./src/middlewares/middleware');
 
 app.use(express.urlencoded({extended: true }));
 
@@ -10,6 +11,8 @@ app.use(express.static(path.resolve(__dirname, 'public')));
 app.set('views', path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
 
+// Nossos próprios middlewares
+app.use(middlewareGlobal);
 app.use(routes); 
 
 app.listen(3000, () => {
