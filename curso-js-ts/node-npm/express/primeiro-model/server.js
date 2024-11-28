9458501c6a5975b5e431db4b59de6a340e218403
Laura Barauna <lauravbarauna@gmail.com>
2024-11-28 18:04:54 -0300

    Finalizando aula sobre primeira conexão com banco de dados e primeiro model

--- curso-js-ts/node-npm/express/primeiro-model/server.js
+++ curso-js-ts/node-npm/express/primeiro-model/server.js
@@ -1,5 +1,15 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.CONNECTIONSTRINGS)
+    .then(() => {
+        app.emit('pronto');
+    })
+    .catch(e => console.log(e));
+
 const routes = require('./routes');
 const path = require('path');
 const { middlewareGlobal, outroMiddleware } = require('./src/middlewares/middleware');
@@ -15,6 +25,9 @@ app.set('view engine', 'ejs');
 app.use(middlewareGlobal);
 app.use(routes); 
 
-app.listen(3000, () => {
-    console.log('Servidor executando na porta 3000')
-});
+app.on('pronto', () => {
+    app.listen(3000, () => {
+        console.log('Servidor executando na porta 3000')
+    });
+})
+
--- curso-js-ts/node-npm/express/primeiro-model/src/controllers/homeController.js
+++ curso-js-ts/node-npm/express/primeiro-model/src/controllers/homeController.js
@@ -1,3 +1,12 @@
+const HomeModel = require('../models/HomeModel');
+
+HomeModel.create({
+    titulo: 'Outra coisa qualquer',
+    descricao: 'Outra descrição.'
+})
+    .then(dados => console.log(dados))
+    .catch(err => console.log(err));
+
 exports.paginaInicial = (req, res,) => {
     res.render('index');
     return;
