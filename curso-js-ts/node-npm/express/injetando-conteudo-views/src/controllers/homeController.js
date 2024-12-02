8cfbbcbebda7d1690105f0e06fe5c926e1449909
Laura Barauna <lauravbarauna@gmail.com>
2024-12-02 19:14:23 -0300

    Finalizando aula de como injetar conteúdo nos views

--- curso-js-ts/node-npm/express/injetando-conteudo-views/src/controllers/homeController.js
+++ curso-js-ts/node-npm/express/injetando-conteudo-views/src/controllers/homeController.js
@@ -1,6 +1,8 @@
 exports.paginaInicial = (req, res,) => {
-    console.log(req.flash('error'), req.flash('succes'), req.flash('info'));
-    res.render('index');
+    res.render('index', {
+        titulo: 'Este será o <span style="color:red;"> título </span> da página',
+        numeros: [0,1,2,3,4,5,6,7,8,9]
+    });
     return;
 };
 
--- curso-js-ts/node-npm/express/injetando-conteudo-views/src/middlewares/middleware.js
+++ curso-js-ts/node-npm/express/injetando-conteudo-views/src/middlewares/middleware.js
@@ -1,4 +1,5 @@
 exports.middlewareGlobal = (req, res, next) => {
+    res.locals.umaVariavelLocal = 'Este é o valor da variável local.';
     next();
 };
 
