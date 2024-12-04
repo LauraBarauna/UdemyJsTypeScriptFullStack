5c3fa8d19d2dafe08bd0fb88d0b42df5b5f84847
Laura Barauna <lauravbarauna@gmail.com>
2024-12-04 18:57:51 -0300

    Começando aula de criação de página de login e cadastro

--- curso-js-ts/projeto-agenda/routes.js
+++ curso-js-ts/projeto-agenda/routes.js
@@ -4,10 +4,6 @@ const homeController = require('./src/controllers/homeController');
 const contatoController = require('./src/controllers/contatoController');
 
 // Rotas da home
-route.get('/', homeController.paginaInicial);
-route.post('/', homeController.trataPost);
-
-// Rotas de contato
-route.get('/contato', contatoController.paginaInicial);
+route.get('/', homeController.index);
 
 module.exports = route; 
--- curso-js-ts/projeto-agenda/src/controllers/contatoController.js
+++
@@ -1,3 +0,0 @@
-exports.paginaInicial = (req, res) => {
-    res.send('Obrigada por entrar em contato')
-}
