3a4b8a7c6492cdc51757314aed27efffb661fd18
Laura Barauna <lauravbarauna@gmail.com>
2024-12-04 19:56:17 -0300

    Começando a criar cadastro de usuários

--- curso-js-ts/projeto-agenda/src/controllers/loginController.js
+++ curso-js-ts/projeto-agenda/src/controllers/loginController.js
@@ -1,3 +1,20 @@
+const Login = require('../models/loginModel');
+
 exports.index = (req, res) => {
     res.render('login');
+};
+
+exports.register = async (req, res) => {
+    const login = new Login(req.body);
+    await login.register();
+
+    if(login.errors.length > 0) {
+        req.flash('errors', login.errors);
+        req.session.save(function () {
+           return  res.redirect('back');
+        });
+        return;
+    }
+
+    res.send(login.errors);
 };
