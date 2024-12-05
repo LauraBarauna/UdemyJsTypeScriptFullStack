d8e45b15f79caf106c4236c00f09cff7a2331e1a
Laura Barauna <lauravbarauna@gmail.com>
2024-12-05 20:22:22 -0300

    Continuando cadastr de usuários

--- curso-js-ts/projeto-agenda/src/controllers/loginController.js
+++ curso-js-ts/projeto-agenda/src/controllers/loginController.js
@@ -5,16 +5,24 @@ exports.index = (req, res) => {
 };
 
 exports.register = async (req, res) => {
-    const login = new Login(req.body);
-    await login.register();
-
-    if(login.errors.length > 0) {
-        req.flash('errors', login.errors);
-        req.session.save(function () {
-           return  res.redirect('back');
+    try {
+        const login = new Login(req.body);
+        await login.register();
+    
+        if(login.errors.length > 0) {
+          req.flash('errors', login.errors);
+          req.session.save(function() {
+            return res.redirect('/login/index');
+          });
+          return;
+        }
+    
+        req.flash('success', 'Seu usuário foi criado com sucesso.');
+        req.session.save(function() {
+          return res.redirect('/login/index');
         });
-        return;
-    }
-
-    res.send(login.errors);
-};
+      } catch(e) {
+        console.log(e);
+        return res.render('404');
+      }
+    };
