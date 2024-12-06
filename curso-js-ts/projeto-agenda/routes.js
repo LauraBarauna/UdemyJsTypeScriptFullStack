fe80ae2147d78fbdcc8082809ca27aa01bb1ef4a
Laura Barauna <lauravbarauna@gmail.com>
2024-12-06 18:22:51 -0300

    Finalizando login de usuários

--- curso-js-ts/projeto-agenda/routes.js
+++ curso-js-ts/projeto-agenda/routes.js
@@ -9,7 +9,9 @@ const loginController = require('./src/controllers/loginController');
 route.get('/', homeController.index);
 
 // Rotas de login
-route.get('/login/index', loginController.index)
-route.post('/login/register', loginController.register)
+route.get('/login/index', loginController.index);
+route.post('/login/register', loginController.register);
+route.post('/login/login', loginController.login);
+route.get('/login/logout', loginController.logout);
 
 module.exports = route; 
