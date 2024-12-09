eb143d027c912fd2e2f1c0e6942fc264a155b8dd
Laura Barauna <lauravbarauna@gmail.com>
2024-12-09 20:08:15 -0300

    Criando e finalizando parte de criar um usuário na agenda (CREATE)

--- curso-js-ts/projeto-agenda/routes.js
+++ curso-js-ts/projeto-agenda/routes.js
@@ -3,6 +3,9 @@ const route = express.Router();
 
 const homeController = require('./src/controllers/homeController');
 const loginController = require('./src/controllers/loginController');
+const contatoController = require('./src/controllers/contatoController');
+
+const { loginRequired } = require('./src/middlewares/middleware')
 
 
 // Rotas da home
@@ -14,4 +17,9 @@ route.post('/login/register', loginController.register);
 route.post('/login/login', loginController.login);
 route.get('/login/logout', loginController.logout);
 
-module.exports = route; 
+// Rotas de contato
+route.get('/contato/index', loginRequired ,contatoController.index);
+route.post('/contato/register', loginRequired ,contatoController.register);
+route.get('/contato/index/:id', loginRequired, contatoController.editIndex);
+
+module.exports = route;
